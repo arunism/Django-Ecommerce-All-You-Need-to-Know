23ddcc712c44139a62f1746a0b0d56b77481fba2
hlwarun <hlwarun@gmail.com>
2021-02-10 21:15:18 +0545

    Adding Route to the app

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,11 @@ from product import views
 app_name = 'product'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/list/', views.product_list, name='product_list'),
+    path('product/details/', views.product_details, name='product_details'),
+    path('cart/', views.cart, name='cart'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('orders/', views.orders, name='orders'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
 ]
--- product/views.py
+++ product/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 def home(request):
     context = {'title': 'Home', 'subtitle':'Products'}
     return render(request, 'index.html', context)
+
+def product_list(request):
+    context = {'title':'Products'}
+    return render(request, 'product-list.html', context)
+
+def product_details(request):
+    context = {'title':'Details', 'subtitle':'Products'}
+    return render(request, 'product-detail.html', context)
+
+def cart(request):
+    context = {'title':'Cart', 'subtitle':'Products'}
+    return render(request, 'cart.html', context)
+
+def wishlist(request):
+    context = {'title':'Wishlist', 'subtitle':'Products'}
+    return render(request, 'wishlist.html', context)
+
+def orders(request):
+    context = {'title':'Orders', 'subtitle':'Products'}
+    return render(request, 'my-account.html', context)
+
+def checkout(request):
+    context = {'title':'Checkout', 'subtitle':'Products'}
+    return render(request, 'checkout.html', context)
+
+def contact(request):
+    context = {'title':'Contact'}
+    return render(request, 'contact.html', context)
